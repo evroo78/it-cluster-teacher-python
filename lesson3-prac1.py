541ec70ea018cb6cb94a42248b237a14a02dafa8
Hanna Shevliakova <evro78@gmail.com>
2023-04-07 18:57:53 +0300

    fixed files for lesson3

--- lesson3-prac1.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-
-if n%2:
-	print(f'{n} is odd number')
-else:
-	print(f'{n} is even number')
--- lesson3-prac2.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-
-if n%2 and (not n%3) and (not n%5) and n%10:
-	print(f'{n} satisfies the conditions')
-else:
-	print(f'{n} does not satisfy the conditions')
